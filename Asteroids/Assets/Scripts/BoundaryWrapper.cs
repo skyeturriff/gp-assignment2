521f5ce03eb0438745938b88551eb971c00ca7a8
skyeturriff <turriff.skye@gmail.com>
2016-03-04 18:53:12 -0500

    Implement boundary wrapping

---
+++ Asteroids/Assets/Scripts/BoundaryWrapper.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * This script controls behaviour when a GameObject goes over the edge of
+ * the screen. It causes the GameObject to wrap around to the opposite screen 
+ * boundary, while maintaining current direction.
+ */
+public class BoundaryWrapper : MonoBehaviour 
+{
+    public Camera mainCamera;   // Camera to query screen bounds
+    Vector2 screenSize;     // Holds screen bounds information
+
+	void Start () 
+    {
+        screenSize.y = mainCamera.orthographicSize;     // half screen height
+        screenSize.x = screenSize.y * mainCamera.aspect;    // half screen width
+	}
+
+
+    // Check if GameObject is out of screen bounds; if so, transport 
+    // it to the opposite side of the screen
+	void Update () 
+    {
+        if (transform.position.x > screenSize.x)
+            transform.position = new Vector3(-screenSize.x, transform.position.y);
+
+        else if (transform.position.x < -screenSize.x)
+            transform.position = new Vector3(screenSize.x, transform.position.y);
+
+        else if (transform.position.y > screenSize.y)
+            transform.position = new Vector3(transform.position.x, -screenSize.y);
+
+        else if (transform.position.y < -screenSize.y)
+            transform.position = new Vector3(transform.position.x, screenSize.y);
+	}
+}
--- Asteroids/Assets/Scripts/PlayerController.cs
+++ Asteroids/Assets/Scripts/PlayerController.cs
@@ -5,32 +5,31 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;         // Allows physics actions to be applied to player 
     float movementSpeed;    // Controls magnitude of force applied to player
+    float rotationSpeed;    
+
 
 	void Start () 
     {
         rb = GetComponent<Rigidbody2D>();
         movementSpeed = 5.0f;
+        rotationSpeed = 3.0f;
 	}
 
-	
-	void Update () 
+
+    void Update()
     {
         // Check for change in player's direction (rotation)
         float moveHorizontal = Input.GetAxis("Horizontal");
 
         // Rotate the player left or right
-        transform.Rotate(transform.forward, -moveHorizontal);
+        transform.Rotate(transform.forward, -moveHorizontal * rotationSpeed); 
     }
 
 
     void FixedUpdate()
     {
-        // Apply force (accelleration/decelleration) to player
-        // in the direction the player is currently facing
+        // Apply force in the direction the player is currently facing
         if (Input.GetKey(KeyCode.UpArrow))
             rb.AddForce(transform.up * movementSpeed);
-        
-        //else if (Input.GetKey(KeyCode.DownArrow))
-        //    rb.AddForce(-transform.up * (movementSpeed / 2.0f));
     }
 }
