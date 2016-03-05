317b2e2657e1f6e5facf8cd03c78efb76ed00635
skyeturriff <turriff.skye@gmail.com>
2016-03-05 00:02:14 -0500

    Track score

--- Asteroids/Assets/Scripts/EnemyController.cs
+++ Asteroids/Assets/Scripts/EnemyController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 /*
  * This script controls behaviour of Enemies. On instantiation, they
@@ -8,13 +9,17 @@ using System.Collections;
  */
 public class EnemyController : MonoBehaviour 
 {
-    Vector2 screenSize;     // Holds screen bounds information
-    float maxSpeed;         // Range for random speed of Enemy motion
+    GameController gameController;  // Reference to the GameController object in this scene
+    Vector2 screenSize;             // Holds screen bounds information
+    float maxSpeed;                 // Range for random speed of Enemy motion
     float minSpeed;
     Rigidbody2D rb;
 
     void Start()
     {
+        // Set gameController to GameController object active in scene
+        gameController = GameObject.FindWithTag("GameController").GetComponent<GameController>();
+
         // Position Enemy randomly within screen bounds
         screenSize.y = Camera.main.orthographicSize;
         screenSize.x = screenSize.y * Camera.main.aspect;
@@ -30,16 +35,6 @@ public class EnemyController : MonoBehaviour
         rb.AddForce(transform.position * Random.Range(minSpeed, maxSpeed));
     }
 
-	void Update () 
-    {
-	
-	}
-
-    void FixedUpdate()
-    {
-
-    }
-
     // If GameObject is hit with a projectile, both the GameObject
     // and the Projectile are destroyed and the Player gains a point
     void OnTriggerEnter2D(Collider2D collider)
@@ -48,8 +43,7 @@ public class EnemyController : MonoBehaviour
         {
             Destroy(gameObject);
             Destroy(collider.gameObject);
-
-            // ADD POINTS
+            gameController.AddPoint();
         }
     }
 }
--- Asteroids/Assets/Scripts/GameController.cs
+++ Asteroids/Assets/Scripts/GameController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 /*
  * This script is primarily responsible for spawning Enemies. Enemies are 
@@ -7,19 +8,24 @@ using System.Collections;
  */
 public class GameController : MonoBehaviour 
 {
-    int waveSize;   // The number of enemies spawned for each wave
-    float spawnInterval;    // Length of time between waves
-    float lastSpawn;    // Time of most recent wave 
+    int waveSize;               // The number of enemies spawned for each wave
+    float spawnInterval;        // Length of time between waves
+    float lastSpawn;            // Time of most recent wave 
     public GameObject enemy;    // Reference to enemy prefab
+    public Text scoreText;      // Displays current score to user
+    int score;                  // Holds the user's current score
 
     void Start()
     {
+        score = 0;
+        UpdateScore();
         waveSize = 5;
         spawnInterval = 7.0f;
         SpawnEnemyWave();
         lastSpawn = Time.time;
     }
 
+    // Check time since last enemy wave spawn
     void Update()
     {
         float timeSinceLastSpawn = Time.time - lastSpawn;
@@ -30,10 +36,24 @@ public class GameController : MonoBehaviour
         }
     }
 
+    // Spawn a new wave of Enemy objects
     void SpawnEnemyWave()
     {
         for (int i = 0; i < waveSize; i++)
             GameObject.Instantiate(enemy);
+    }
+
+    // Increment the user's score
+    public void AddPoint()
+    {
+        ++score;
+        UpdateScore();
+    }
+
+    // Update score display
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score.ToString();
     }
 	
 }
